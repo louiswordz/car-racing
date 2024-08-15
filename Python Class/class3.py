854b1c6ea4ba1435c0a2183842acec90ccae0832
unknown <louismerit3@gmail.com>
2024-08-15 12:28:30 +0100

    game enhancement

--- Python Class/class3.py
+++ Python Class/class3.py
@@ -1,7 +1,17 @@
+
 class Animal:
     def __init__(self, name, legs):
         self.name = name
         self.legs = legs
+        self.eat = ["Grass", "Dungs","Leaf"]
         
     def Running(self):
-        return f"{self.name}"
+        return f"{self.name} is running on {self.legs} legs"
+    
+    def Eating (self):
+        for i in self.eat:
+            return f"{self.name} eats {i}"
+   
+A = Animal("Cow", 4)
+print(A.Running())
+print(A.Eating())
